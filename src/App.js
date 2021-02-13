d8d609f26792b04fdb53a964257c90cbf81d825c
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2021-02-13 03:41:13 -0900

    Router, Layouts and Footer created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import Header from './components/Header';
+import { Route, Switch } from 'react-router-dom';
+//layouts
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+//pages
 import HomePage from './pages/HomePage';
+import Registration from './pages/Registration';
 import './default.scss';
+
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <HomePage />
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => (
+          <HomepageLayout>
+            <HomePage />
+          </HomepageLayout>
+        )} />
+        <Route path="/registration" render={() => (
+          <MainLayout>
+            <Registration />
+          </MainLayout>
+        )} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/Registration/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Registration = () => {
+    return (
+        <div>
+            <h1>
+                Registration Page
+            </h1>
+        </div>
+    );
+};
+
+export default Registration;
