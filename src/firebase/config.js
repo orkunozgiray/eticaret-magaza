800f2f158a70f08d2480b80e4818adb35ddf04de
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2021-02-14 07:20:06 -0900

    Firebase Login and Database Set

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBwuzKiLhxal-d1sznC5uDVwxBaUp7Po74",
+  authDomain: "eticaret-magaza.firebaseapp.com",
+  projectId: "eticaret-magaza",
+  storageBucket: "eticaret-magaza.appspot.com",
+  messagingSenderId: "309727914468",
+  appId: "1:309727914468:web:5b4ac4af391fc48127bff1",
+  measurementId: "G-LV8HQ83CXP"
+};
