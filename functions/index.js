6ce864950e87cc8d10624bb3b99a458fed928ac0
orkunozgiray <36546670+orkunozgiray@users.noreply.github.com>
2021-02-26 12:03:01 -0900

    Stripe Payment

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
+const stripe = require('stripe')('sk_test_51FxNB4EVqFiUSN9e7ofxFgpHQrVpFMz1nbwb9lcFSosGgrtSKgrb4YV3npeLP2DM3I9gg4UsRT9cYxt6JBJmgx5y00ZULKqAOE');
 
 const app = express();
 
@@ -8,6 +9,29 @@ app.use(cors({
   origin: true,
 }));
 app.use(express.json());
+
+app.post('/payments/create', async (req, res) => {
+  try {
+    const { amount, shipping } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      shipping,
+      amount,
+      currency: 'usd'
+    });
+
+    res
+      .status(200)
+      .send(paymentIntent.client_secret);
+
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        statusCode: 500,
+        message: err.message
+      });
+  }
+})
 
 app.get("*", (req, res) => {
   res
